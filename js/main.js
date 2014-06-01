fb8a5ae04ca29af1c7a8bc78643804d1d03cbd5e
Jair Santos <jairdesouzasantos@gmail.com>
2014-06-01 15:26:56 -0300

    Change to use angular_br

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 require.config({
   paths: {
     "jquery"           : "../lib/jquery/dist/jquery",
-    "angular"          : "../lib/angular/angular",
+    "angular"          : "../lib/angular/angular_br",
     "angular-route"    : "../lib/angular-route/angular-route",
     "twitterbootstrap" : "../lib/bootstrap-3.1.1-dist/js/bootstrap",
     "jstree"           : "../lib/vakata-jstree-e22db21/dist/jstree",
